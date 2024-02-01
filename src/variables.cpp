b84a2c344c9771cfd544f948d3262d51ea5cfbee
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2024-02-01 17:34:01 +0000

    Changed ports again

--- src/variables.cpp
+++ src/variables.cpp
@@ -9,15 +9,15 @@
 //Initialise devices
 pros::Controller controller(CONTROLLER_MASTER);	
 
-pros::Motor topLeft(19, MOTOR_GEAR_BLUE, true);
-pros::Motor topRight(18, MOTOR_GEAR_BLUE, false);
+pros::Motor topLeft(9, MOTOR_GEAR_BLUE, true);
+pros::Motor topRight(13, MOTOR_GEAR_BLUE, false);
 
 pros::Motor catapultLeft(10, MOTOR_GEAR_GREEN, true);
-pros::Motor catapultRight(20, pros::E_MOTOR_GEAR_GREEN, true);
+pros::Motor catapultRight(2, pros::E_MOTOR_GEAR_GREEN, false);
 pros::Motor_Group catapult_motors({catapultLeft, catapultRight});
 
-pros::Motor bottomLeft(8, MOTOR_GEAR_BLUE, true);
-pros::Motor bottomRight(7, MOTOR_GEARSET_18, false);
+pros::Motor bottomLeft(11, MOTOR_GEAR_BLUE, true);
+pros::Motor bottomRight(1, MOTOR_GEARSET_18, false);
 
 pros::Motor_Group left_motors({topLeft, bottomLeft});
 pros::Motor_Group right_motors({topRight, bottomRight});
@@ -27,7 +27,7 @@ std::atomic_bool is_PTO_on_base = true;
 
 pros::Motor intake(0, MOTOR_GEAR_BLUE, false);
 
-pros::Motor flywheel(11, pros::E_MOTOR_GEAR_BLUE);
+pros::Motor flywheel(0, pros::E_MOTOR_GEAR_BLUE);
 pros::ADIDigitalOut wings('F');
 
 //Initialise sensors
