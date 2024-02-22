c7f6e2c0209e9a48ec488a54a662951f35c9373c
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2024-02-22 09:58:44 +0000

    Changed ports

--- src/variables.cpp
+++ src/variables.cpp
@@ -9,15 +9,15 @@
 //Initialise devices
 pros::Controller controller(CONTROLLER_MASTER);	
 
-pros::Motor topLeft(3, MOTOR_GEAR_BLUE, true);
-pros::Motor topRight(4, MOTOR_GEAR_BLUE, false);
+pros::Motor topLeft(9, MOTOR_GEAR_BLUE, true);
+pros::Motor topRight(1, MOTOR_GEAR_BLUE, false);
 
-pros::Motor catapultLeft(0, MOTOR_GEAR_GREEN, true);
-pros::Motor catapultRight(0, pros::E_MOTOR_GEAR_GREEN, false);
+pros::Motor catapultLeft(20, MOTOR_GEAR_BLUE, true);
+pros::Motor catapultRight(6, pros::E_MOTOR_GEAR_BLUE, false);
 pros::Motor_Group catapult_motors({catapultLeft, catapultRight});
 
-pros::Motor bottomLeft(2, MOTOR_GEAR_BLUE, true);
-pros::Motor bottomRight(11, MOTOR_GEAR_BLUE, false);
+pros::Motor bottomLeft(10, MOTOR_GEAR_BLUE, true);
+pros::Motor bottomRight(2, MOTOR_GEAR_BLUE, false);
 
 pros::Motor_Group left_motors({topLeft, bottomLeft});
 pros::Motor_Group right_motors({topRight, bottomRight});
@@ -31,6 +31,6 @@ pros::Motor flywheel(0, pros::E_MOTOR_GEAR_BLUE);
 pros::ADIDigitalOut wings('H');
 
 //Initialise sensors
-pros::Imu inertial(19);
-pros::Rotation right_tracker(1);
-pros::Rotation back_tracker(13, true);
+pros::Imu inertial(8);
+pros::Rotation right_tracker(3);
+pros::Rotation back_tracker(4, true);
