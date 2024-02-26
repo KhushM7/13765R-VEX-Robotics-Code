6bfaee20ec07963988d53dd572b4e3d27b1571c1
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2024-02-26 16:18:28 +0000

    port change

--- src/variables.cpp
+++ src/variables.cpp
@@ -9,15 +9,15 @@
 //Initialise devices
 pros::Controller controller(CONTROLLER_MASTER);	
 
-pros::Motor topLeft(9, MOTOR_GEAR_BLUE, true);
+pros::Motor topLeft(8, MOTOR_GEAR_BLUE, true);
 pros::Motor topRight(1, MOTOR_GEAR_BLUE, false);
 
-pros::Motor catapultLeft(19, MOTOR_GEAR_BLUE, false);
-pros::Motor catapultRight(6, pros::E_MOTOR_GEAR_BLUE, true);
+pros::Motor catapultLeft(7, MOTOR_GEAR_BLUE, false);
+pros::Motor catapultRight(3, pros::E_MOTOR_GEAR_BLUE, true);
 pros::Motor_Group catapult_motors({catapultLeft, catapultRight});
 
-pros::Motor bottomLeft(10, MOTOR_GEAR_BLUE, true);
-pros::Motor bottomRight(2, MOTOR_GEAR_BLUE, false);
+pros::Motor bottomLeft(9, MOTOR_GEAR_BLUE, true);
+pros::Motor bottomRight(6, MOTOR_GEAR_BLUE, false);
 
 pros::Motor_Group left_motors({topLeft, bottomLeft});
 pros::Motor_Group right_motors({topRight, bottomRight});
@@ -25,12 +25,12 @@ pros::Motor_Group right_motors({topRight, bottomRight});
 pros::ADIDigitalOut PTOpiston('E');
 std::atomic_bool is_PTO_on_base = true;
 
-pros::Motor intake(11, MOTOR_GEAR_BLUE, false);
+pros::Motor intake(2, MOTOR_GEAR_BLUE, false);
 
 pros::Motor flywheel(0, pros::E_MOTOR_GEAR_BLUE);
 pros::ADIDigitalOut wings('A');
 
 //Initialise sensors
-pros::Imu inertial(8);
-pros::Rotation right_tracker(3);
-pros::Rotation back_tracker(4, true);
+pros::Imu inertial(20);
+pros::Rotation right_tracker(4);
+pros::Rotation back_tracker(10, true);
